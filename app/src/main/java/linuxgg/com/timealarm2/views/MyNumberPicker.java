b1bfccb76827c86c3c9df2574d5f0609141dba6d
tom <qdtom@foxmail.com>
2016-10-06 23:16:14 +0800

    change NumberPicker to bigger

---
+++ app/src/main/java/linuxgg/com/timealarm2/views/MyNumberPicker.java
@@ -0,0 +1,71 @@
+package linuxgg.com.timealarm2.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+
+/**
+ * Created by tom on 2016/10/6.<p>
+ * Version 1.0 <p>
+ * Desc :    <p>
+ */
+public class MyNumberPicker extends NumberPicker {
+
+
+    public MyNumberPicker(Context context) {
+        super(context);
+    }
+
+    public MyNumberPicker(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyNumberPicker(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public MyNumberPicker(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public void addView(View child) {
+        super.addView(child);
+        updateView(child);
+    }
+
+    @Override
+    public void addView(View child, int index) {
+        super.addView(child, index);
+        updateView(child);
+    }
+
+    @Override
+    public void addView(View child, int index, ViewGroup.LayoutParams params) {
+        super.addView(child, index, params);
+        updateView(child);
+    }
+
+    @Override
+    public void addView(View child, ViewGroup.LayoutParams params) {
+        super.addView(child, params);
+        updateView(child);
+    }
+
+    @Override
+    public void addView(View child, int width, int height) {
+        super.addView(child, width, height);
+        updateView(child);
+    }
+
+    private void updateView(View child) {
+
+        if (child instanceof EditText) {
+            ((EditText) child).setTextSize(30);
+        }
+
+    }
+}
